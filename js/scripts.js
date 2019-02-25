1b9eaf1a75f194288c9229b287d067e58f87e531
santa <santa@northpole.com>
2019-02-25 09:50:59 +0300

    change the background image

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,17 @@ $(document).ready(function() {
 
   $("form#formquiz").submit(function(event){
 
-    var Q1= ($("input:radio[name=operator]:checked").val());
-    var Q2= ($("input:radio[name=types]:checked").val());
-    var Q3= ($("input:radio[name=parenthesis]:checked").val());
-    var Q4= ($("input:radio[name=comparison]:checked").val());
-    var Q5= ($("input:radio[name=naming]:checked").val());
+    var Q1= $("input:radio[name=operator]:checked").val();
+    var Q2= $("input:radio[name=types]:checked").val();
+    var Q3= $("input:radio[name=parenthesis]:checked").val();
+    var Q4= $("input:radio[name=comparison]:checked").val();
+    var Q5= $("input:radio[name=naming]:checked").val();
 
     var total=parseInt(Q1)+parseInt(Q2)+parseInt(Q3)+parseInt(Q4)+parseInt(Q5);
     $("#total").text("Congratulations!"+"this is your score"+ total);
 
 
   //==== user Interface ====//
-
-  $("form#formquiz").slideup(1350);
   $("#total").show(3000);
    event.preventDefault();
  });
